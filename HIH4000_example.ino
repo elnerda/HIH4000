75f149dde32c0c2f9fa15e2b2c407f7aef7bbc43
Gaetano Fresa <fischkotpf@hotmail.de>
2016-10-27 17:38:49 +0200

    Update HIH4000_example.ino

--- HIH4000_example.ino
+++ HIH4000_example.ino
@@ -1,8 +1,8 @@
 #include <HIH4000.h>
 
 int readPin=A3;
-HIH4000 hih(readPin,25.0);
-
+HIH4000 hih(readPin); //the analog pin where the sensor is connected
+float tempC=44.23;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,7 +10,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- float rh= hih.getHumidity();
+ float rh= hih.getHumidity(); //humidity with compensation for 25C(typical use)
  Serial.println(rh);
+ float truerh=hih.getTrueHumidity(tempC); //function for temperatue compensated Humidity, tempC can be read from sensor
+ Serial.println(truerh);                  //make sure to read temepratue first, dann call .getTrueHumidity
  delay(600);
 }
