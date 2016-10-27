4ff10c93a51b9347acae3d72e3c9bcfe7c80c3a5
Gaetano Fresa <fischkotpf@hotmail.de>
2016-10-27 17:22:21 +0200

    Rename HIH4000.ino to HIH4000_example.ino

---
+++ HIH4000_example.ino
@@ -0,0 +1,16 @@
+#include <HIH4000.h>
+
+int readPin=A3;
+HIH4000 hih(readPin,25.0);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ float rh= hih.getHumidity();
+ Serial.println(rh);
+ delay(600);
+}
